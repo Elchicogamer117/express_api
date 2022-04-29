4a3954d3de79013c6bc466304771e41441d89cac
Cristian <cris18002011@hotmail.com>
2022-04-28 20:37:35 -0500

    Paso 7: Creando y probando un endpoint para eliminar explorer - DELETE

--- app.js
+++ app.js
@@ -29,14 +29,21 @@ app.post('/v1/explorers', (req, res) => {
       console.log(`Api explorers POST request ${new Date()}`)
       //Agregando la logica para persistir
       const requestBody = req.body //*? Parámetros de un cliente
-      res.status(201).json({message: "Created"})
+      res.status(201).json({message: "Created ✔"})
 })
 
 app.put('/v1/explorers/:id', (req, res) => {
-      console.log(`Api Explorers PUT request ${new Date()}`)
+      console.log(`Api explorers PUT request ${new Date()}`)
       console.log(`Update explorer with id ${req.params.id}`)
       const requestBody = req.body
-      res.status(200).json({message: "Updated!"})
+      res.status(200).json({message: "Updated! ✔"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+      console.log(`Api explorers DELETE request ${new Date()}`)
+      console.log(`Delete explorer with id ${req.params.id}`)
+      const requestBody = req.body 
+      res.status(200).json({message: "Deleted ✖"})
 })
 
 //* Inicializamos la app
