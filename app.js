f8cd27a898e7690cd9b5cf9355d6829344ef0fc4
Cristian <cris18002011@hotmail.com>
2022-04-28 19:42:20 -0500

    Paso 3: Creando y probando(postman) un endpoint que regrese una lista de explorers - GET

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const app = express()
 //* Puerto en que vamos a ver nuestra app -> localhost:3000
 const port = 3000
 
+//* HTTP Methods
+//get
+app.get('/v1/explorers', (req, res) => {
+      console.log(`Api explorers GET ALL requests ${new Date()}`)
+      const explorer1 = {id:1, name: 'Cristian'}
+      const explorer2 = {id:117, name: 'John'}
+      const explorer3 = {id:2, name: 'Cortana'}
+      const explorer4 = {id:4, name: 'Halsey'}
+      const explorer5 = {id:217, name: 'Fernando'}
+      const explorers = [explorer1, explorer2, explorer3, explorer4, explorer5]
+      res.status(200).json(explorers)
+})
+
 //* Inicializamos la app
 app.listen(port, () => {
       console.log(`Example app listening on port: ${port}`)
